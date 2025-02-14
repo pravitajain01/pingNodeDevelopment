15c3e0c5f0068d897a95ef1abad2d818b34b9a29
Pravita Jain <pravita.jain@metacube.com>
2025-02-14 16:47:11 +0530

    added other pages

--- server.js
+++ server.js
@@ -1,60 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const fetch = require('node-fetch');
-// const app = express();
-// const path = require('path');
-// // const fs = require('fs');
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Serve your HTML file
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// // const COMPANY_ID = '2422107b-7011-48bb-8366-735e927271f2';
-// // const API_KEY = '67325a24e28d69f37f5c49a66cfe4771d5ad368e0a7641c20b099418f69fcc28bfaf7bd2e0d2c0ef5f0eb42e722efd2354b2b4d6ef67c1f5426dc39ba3af8180b5e8ef2007e596822ce0d31a037baa0b674c7468e7a1ed7628cfdfecb8ecce5db58597dbab62944c0f431ef8fe94797f9968f0980678bd610973d25df2e34c13';
-// // const REGION = 'com'; // e.g., 'com' for North America
-
-// // // const davinci = fs.readFileSync('public/js/davinci.js','utf8');
-// // // eval(davinci);
-
-
-// // app.get('/get-sdk-token', async (req, res) => {
-  
-// //   try {
-// //     const url = `https://orchestrate-api.pingone.${REGION}/v1/company/${COMPANY_ID}/sdktoken`;
-// //     const response = await fetch(url, {
-// //       headers: { 'X-SK-API-KEY': API_KEY, },
-// //     });
-
-// //     if(!response.ok){
-// //         throw new Error(`Pingone API error : ${response.statusText}`);
-// //     }
-// //     const data = await response.json();
-// //     res.json(data);
-// //   } catch (error) {
-// //     console.error('Error fetching SDK token:', error);
-// //     res.status(500).json({error: 'Failed to retrieve SDK token'});
-// //   }
-// // });
-
-
-// // const PORT = process.env.PORT || 3000;
-// app.listen(3000, () => {
-//   console.log('Server running on http://localhost:3000');
-// });
-
-
-
-//  Copyright © 2019 Ping Identity. All rights reserved.
-//
-//  This software may be modified and distributed under the terms
-//  of the MIT license. See the LICENSE file for details.
-//
 const http = require('http');
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
@@ -213,6 +156,20 @@ app.get('/dashboard', function(req, res){
 // Website default page
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+//website create account page
+app.get('/create-account', function(req, res){
+    // Re-authenticate if no session is found
+    if (!req.session.data) {
+        return res.redirect('login');
+    }
+
+    // Session exists so render dashboard
+    res.render('create-account', {
+        accessToken: req.session.data.accessToken,
+        idToken: req.session.data.idToken
+    });
 });
 
 const server = http.createServer(app);
